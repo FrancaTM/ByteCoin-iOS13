cfbbbe5a108357ec66ab5c32eba181b7049eb6e6
FrancaTM <francatm@outlook.com>
2021-02-27 15:47:53 -0300

    update UI using delegate pattern add CoinManagerDelegate

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -14,10 +14,12 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
     
-    let coinManager = CoinManager()
+    var coinManager = CoinManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        coinManager.delegate = self
         
         currencyPicker.dataSource = self
         currencyPicker.delegate = self
@@ -41,3 +43,16 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
 }
 
+// MARK: - CoinManagerDelegate
+extension ViewController: CoinManagerDelegate {
+    func didUpdatePrice(price: String, currency: String) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,19 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "YOUR_API_KEY_HERE"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    var delegate: CoinManagerDelegate?
 
     func getCoinPrice(for currency: String) {
         print(currency)
@@ -29,9 +36,11 @@ struct CoinManager {
                 }
                                 
                 if let safeData = data {
-                    let bitcoinPrice = parseJSON(safeData)
+                    if let bitcoinPrice = parseJSON(safeData) {
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
                 }
-
             }
             
             task.resume()
